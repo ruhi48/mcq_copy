3219ff7bb0f2fefa7d62f6ad3b866acf44cca196
ruhi48 <ruhishaikh912@gmail.com>
2025-04-09 13:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-import streamlit as st
+__import__('pysqlite3')
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import chromadb
+import streamlit as st
+import sys
+import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
